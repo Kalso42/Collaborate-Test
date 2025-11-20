36f8cdab4469b6b076c45ff60e697e16bffa3120
Qimin Zhong <2023044007@email.szu.edu.cn>
2025-11-20 20:44:41 +0800

    Initial commit: 提交OpenGL图形学项目源码

---
+++ TriMesh.cpp
@@ -0,0 +1,157 @@
+﻿#include "include/TriMesh.h"
+#include <fstream>
+#include <sstream>
+#include <algorithm> // 为了使用 std::min 和 std::max
+
+// 一些基础颜色
+const glm::vec3 basic_colors[8] = {
+	glm::vec3(1.0, 1.0, 1.0),	// White
+	glm::vec3(1.0, 1.0, 0.0),	// Yellow
+	glm::vec3(0.0, 1.0, 0.0),	// Green
+	glm::vec3(0.0, 1.0, 1.0),	// Cyan
+	glm::vec3(1.0, 0.0, 1.0),	// Magenta
+	glm::vec3(1.0, 0.0, 0.0),	// Red
+	glm::vec3(0.0, 0.0, 0.0),	// Black
+	glm::vec3(0.0, 0.0, 1.0)	// Blue
+};
+
+// 立方体的各个点
+const glm::vec3 cube_vertices[8] = {
+	glm::vec3(-0.5, -0.5, -0.5), glm::vec3(0.5, -0.5, -0.5),
+	glm::vec3(-0.5,  0.5, -0.5), glm::vec3(0.5,  0.5, -0.5),
+	glm::vec3(-0.5, -0.5,  0.5), glm::vec3(0.5, -0.5,  0.5),
+	glm::vec3(-0.5,  0.5,  0.5), glm::vec3(0.5,  0.5,  0.5)
+};
+
+
+TriMesh::TriMesh() {}
+TriMesh::~TriMesh() {}
+
+std::vector<glm::vec3> TriMesh::getVertexPositions() { return vertex_positions; }
+std::vector<glm::vec3> TriMesh::getVertexColors() { return vertex_colors; }
+std::vector<vec3i> TriMesh::getFaces() { return faces; }
+std::vector<glm::vec3> TriMesh::getPoints() { return points; }
+std::vector<glm::vec3> TriMesh::getColors() { return colors; }
+
+void TriMesh::cleanData() {
+	vertex_positions.clear();
+	vertex_colors.clear();	
+	faces.clear();
+	points.clear();
+	colors.clear();
+}
+
+void TriMesh::storeFacesPoints() {
+    points.clear();
+    colors.clear();
+    for (auto& f : faces) {
+        points.push_back(vertex_positions[f.x]);
+        points.push_back(vertex_positions[f.y]);
+        points.push_back(vertex_positions[f.z]);
+        if (vertex_colors.size() > 0) {
+            colors.push_back(vertex_colors[f.x]);
+            colors.push_back(vertex_colors[f.y]);
+            colors.push_back(vertex_colors[f.z]);
+        }
+    }
+}
+
+void TriMesh::generateCube() {
+	cleanData();
+    for (int i = 0; i < 8; i++) {
+        vertex_positions.push_back(cube_vertices[i]);
+        vertex_colors.push_back(basic_colors[i]);
+    }
+    int cube_faces[12][3] = {
+        {0, 1, 2}, {1, 3, 2}, {4, 6, 5}, {5, 6, 7}, {0, 2, 4}, {4, 2, 6},
+        {1, 5, 3}, {5, 7, 3}, {0, 4, 1}, {1, 4, 5}, {2, 3, 6}, {3, 7, 6}
+    };
+    for (int i = 0; i < 12; i++) {
+        faces.push_back(vec3i(cube_faces[i][0], cube_faces[i][1], cube_faces[i][2]));
+    }
+	storeFacesPoints();
+}
+
+void TriMesh::setColor(const glm::vec3& color) {
+    vertex_colors.clear();
+    for (size_t i = 0; i < vertex_positions.size(); ++i) {
+        vertex_colors.push_back(color);
+    }
+}
+
+void TriMesh::setGradientColor(const glm::vec3& bottom_color, const glm::vec3& top_color) {
+    if (vertex_positions.empty()) { return; }
+    float minY = vertex_positions[0].y;
+    float maxY = vertex_positions[0].y;
+    for (const auto& vertex : vertex_positions) {
+        if (vertex.y < minY) minY = vertex.y;
+        if (vertex.y > maxY) maxY = vertex.y;
+    }
+    vertex_colors.clear();
+    float height = maxY - minY;
+    for (const auto& vertex : vertex_positions) {
+        float t = (height > 0.0001f) ? (vertex.y - minY) / height : 0.5f;
+        vertex_colors.push_back(glm::mix(bottom_color, top_color, t));
+    }
+}
+
+void TriMesh::setPositionalColor() {
+    if (vertex_positions.empty()) {
+        return;
+    }
+
+    glm::vec3 minPos = vertex_positions[0];
+    glm::vec3 maxPos = vertex_positions[0];
+    for (const auto& vertex : vertex_positions) {
+        minPos.x = std::min(minPos.x, vertex.x);
+        minPos.y = std::min(minPos.y, vertex.y);
+        minPos.z = std::min(minPos.z, vertex.z);
+        maxPos.x = std::max(maxPos.x, vertex.x);
+        maxPos.y = std::max(maxPos.y, vertex.y);
+        maxPos.z = std::max(maxPos.z, vertex.z);
+    }
+
+    glm::vec3 size = maxPos - minPos;
+    vertex_colors.clear();
+
+    for (const auto& vertex : vertex_positions) {
+        float red   = (size.x > 0.0001f) ? (vertex.x - minPos.x) / size.x : 0.5f;
+        float green = (size.y > 0.0001f) ? (vertex.y - minPos.y) / size.y : 0.5f;
+        float blue  = (size.z > 0.0001f) ? (vertex.z - minPos.z) / size.z : 0.5f;
+        
+        vertex_colors.push_back(glm::vec3(red, green, blue));
+    }
+}
+
+bool TriMesh::readOff(const std::string& filename)
+{
+    if (filename.empty()) { return false; }
+    std::ifstream fin(filename);
+    if (!fin) {
+        printf("File on error\n");
+        return false;
+    }
+    
+    printf("File open success\n");
+    cleanData();
+    int nVertices, nFaces, nEdges;
+    std::string str;
+    fin >> str;
+    if (str != "OFF") {
+        printf("Not a valid OFF file\n");
+        return false;
+    }
+    fin >> nVertices >> nFaces >> nEdges;
+    for (int i = 0; i < nVertices; i++) {
+        glm::vec3 tmp_node;
+        fin >> tmp_node.x >> tmp_node.y >> tmp_node.z;
+        vertex_positions.push_back(tmp_node);
+    }
+    for (int i = 0; i < nFaces; i++) {
+        int num, a, b, c;
+        fin >> num >> a >> b >> c;
+        faces.push_back(vec3i(a, b, c));
+    }
+    fin.close();
+    return true;
+};
---
+++ main.cpp
@@ -0,0 +1,229 @@
+#include "include/Angel.h"
+#include "include/TriMesh.h"
+
+#include <vector>
+#include <string>
+
+// 常量定义和全局变量
+const int X_AXIS = 0;
+const int Y_AXIS = 1;
+const int Z_AXIS = 2;
+const int TRANSFORM_SCALE = 0;
+const int TRANSFORM_ROTATE = 1;
+const int TRANSFORM_TRANSLATE = 2;
+const double DELTA_DELTA = 0.3;
+const double DEFAULT_DELTA = 0.5;
+
+double scaleDelta = DEFAULT_DELTA;
+double rotateDelta = DEFAULT_DELTA;
+double translateDelta = DEFAULT_DELTA;
+glm::vec3 scaleTheta(1.0, 1.0, 1.0);
+glm::vec3 rotateTheta(0.0, 0.0, 0.0);
+glm::vec3 translateTheta(0.0, 0.0, 0.0);
+int currentTransform = TRANSFORM_ROTATE;
+int mainWindow;
+bool isAnimating = true;
+int rotationAxis = Y_AXIS;
+
+struct openGLObject {
+	GLuint vao, vbo, program;
+	std::string vshader, fshader;
+	GLuint pLocation, cLocation, matrixLocation;
+};
+openGLObject model_object;
+TriMesh* mesh = new TriMesh();
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void bindObjectAndData(TriMesh* mesh, openGLObject& object, const std::string& vshader, const std::string& fshader) {
+    glGenVertexArrays(1, &object.vao);
+	glBindVertexArray(object.vao);
+	glGenBuffers(1, &object.vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, object.vbo);
+	glBufferData(GL_ARRAY_BUFFER,
+		mesh->getPoints().size() * sizeof(glm::vec3) + mesh->getColors().size() * sizeof(glm::vec3),
+		NULL, GL_STATIC_DRAW);
+	glBufferSubData(GL_ARRAY_BUFFER, 0, mesh->getPoints().size() * sizeof(glm::vec3), &mesh->getPoints()[0]);
+	glBufferSubData(GL_ARRAY_BUFFER, mesh->getPoints().size() * sizeof(glm::vec3), mesh->getColors().size() * sizeof(glm::vec3), &mesh->getColors()[0]);
+	object.vshader = vshader;
+	object.fshader = fshader;
+	object.program = InitShader(object.vshader.c_str(), object.fshader.c_str());
+	object.pLocation = glGetAttribLocation(object.program, "vPosition");
+	glEnableVertexAttribArray(object.pLocation);
+	glVertexAttribPointer(object.pLocation, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	object.cLocation = glGetAttribLocation(object.program, "vColor");
+	glEnableVertexAttribArray(object.cLocation);
+	glVertexAttribPointer(object.cLocation, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(mesh->getPoints().size() * sizeof(glm::vec3)));
+	object.matrixLocation = glGetUniformLocation(object.program, "matrix");
+}
+
+void init()
+{
+	std::string vshader, fshader;
+	vshader = "shaders/vshader.glsl";
+	fshader = "shaders/fshader.glsl";
+
+    if (!mesh->readOff("Models/cow.off")) { // <--- 确保文件名和路径正确
+        printf("Failed to read OFF file. Exiting.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // 使用XYZ坐标映射来生成丰富的渐变色
+    mesh->setPositionalColor();
+
+    // 准备好顶点和颜色数据
+    mesh->storeFacesPoints();
+
+	bindObjectAndData(mesh, model_object, vshader, fshader);
+
+	// 设置背景色
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+}
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(model_object.program);
+    glBindVertexArray(model_object.vao);
+	glm::mat4 m(1.0);
+	m = glm::translate(m, translateTheta);
+	m = glm::rotate(m, glm::radians(rotateTheta.x), glm::vec3(1,0,0));
+	m = glm::rotate(m, glm::radians(rotateTheta.y), glm::vec3(0,1,0));
+	m = glm::rotate(m, glm::radians(rotateTheta.z), glm::vec3(0,0,1));
+	m = glm::scale(m, scaleTheta);
+	glUniformMatrix4fv(model_object.matrixLocation, 1, GL_FALSE, glm::value_ptr(m));
+	glDrawArrays(GL_TRIANGLES, 0, mesh->getPoints().size());
+}
+
+void updateTheta(int axis, int sign) {
+	switch (currentTransform) {
+	case TRANSFORM_SCALE: scaleTheta[axis] += sign * scaleDelta; break;
+	case TRANSFORM_ROTATE: rotateTheta[axis] += sign * rotateDelta; break;
+	case TRANSFORM_TRANSLATE: translateTheta[axis] += sign * translateDelta; break;
+	}
+}
+void resetTheta() {
+	scaleTheta = glm::vec3(1.0, 1.0, 1.0);
+	rotateTheta = glm::vec3(0.0, 0.0, 0.0);
+	translateTheta = glm::vec3(0.0, 0.0, 0.0);
+	scaleDelta = DEFAULT_DELTA;
+	rotateDelta = DEFAULT_DELTA;
+	translateDelta = DEFAULT_DELTA;
+}
+void updateDelta(int sign) {
+	switch (currentTransform) {
+	case TRANSFORM_SCALE: scaleDelta += sign * DELTA_DELTA; break;
+	case TRANSFORM_ROTATE: rotateDelta += sign * DELTA_DELTA; break;
+	case TRANSFORM_TRANSLATE: translateDelta += sign * DELTA_DELTA; break;
+	}
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (action != GLFW_PRESS && action != GLFW_REPEAT) { return; }
+	switch (key) {	
+		case GLFW_KEY_ESCAPE: glfwSetWindowShouldClose(window, GL_TRUE); break;
+		case GLFW_KEY_1: currentTransform = TRANSFORM_SCALE; break;
+		case GLFW_KEY_2: currentTransform = TRANSFORM_ROTATE; break;
+		case GLFW_KEY_3: currentTransform = TRANSFORM_TRANSLATE; break;
+		case GLFW_KEY_4: glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); break;
+		case GLFW_KEY_5: glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); break;
+		case GLFW_KEY_Q: updateTheta(X_AXIS, 1); break;
+		case GLFW_KEY_A: updateTheta(X_AXIS, -1); break;
+		case GLFW_KEY_W: updateTheta(Y_AXIS, 1); break;
+		case GLFW_KEY_S: updateTheta(Y_AXIS, -1); break;
+		case GLFW_KEY_E: updateTheta(Z_AXIS, 1); break;
+		case GLFW_KEY_D: updateTheta(Z_AXIS, -1); break;
+		case GLFW_KEY_R: updateDelta(1); break;
+		case GLFW_KEY_F: updateDelta(-1); break;
+		case GLFW_KEY_T: resetTheta(); break;
+        case GLFW_KEY_X: rotationAxis = X_AXIS; break;
+        case GLFW_KEY_Y: rotationAxis = Y_AXIS; break;
+        case GLFW_KEY_Z: rotationAxis = Z_AXIS; break;
+	}
+}
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (action == GLFW_PRESS) {
+        switch (button) {
+            case GLFW_MOUSE_BUTTON_LEFT: isAnimating = true; break;
+            case GLFW_MOUSE_BUTTON_RIGHT: isAnimating = false; break;
+        }
+    }
+}
+
+void printHelp() {
+	printf("%s\n\n", "3D Model Reader and Controller");
+	printf("Mouse options:\n");
+    printf("Left Button:  Start / Resume animation\n");
+    printf("Right Button: Pause animation\n\n");
+	printf("Keyboard options:\n");
+    printf("x/y/z:        Set rotation axis to X, Y, or Z\n");
+	printf("1/2/3:        Switch transform mode (Scale/Rotate/Translate)\n");
+	printf("q/a:          Increase/Decrease X value\n");
+	printf("w/s:          Increase/Decrease Y value\n");
+	printf("e/d:          Increase/Decrease Z value\n");
+	printf("r/f:          Increase/Decrease delta of current transform\n");
+	printf("t:            Reset all transformations\n");
+    printf("4/5:          Switch draw mode (Line/Fill)\n");
+    printf("ESC:          Exit\n");
+}
+
+void cleanData() {
+	mesh->cleanData();
+	delete mesh;
+	mesh = NULL;
+    glDeleteVertexArrays(1, &model_object.vao);
+	glDeleteBuffers(1, &model_object.vbo);
+	glDeleteProgram(model_object.program);
+}
+
+int main(int argc, char** argv)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	#ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "2023044007_ZhongQimin_Lab2", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	init();
+	printHelp();
+	glEnable(GL_DEPTH_TEST);
+	
+	while (!glfwWindowShouldClose(window))
+	{
+        if (isAnimating) {
+            rotateTheta[rotationAxis] += rotateDelta;
+            if (rotateTheta[rotationAxis] > 360.0f) {
+                rotateTheta[rotationAxis] -= 360.0f;
+            }
+        }
+		display();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	cleanData();
+	return 0;
+}
